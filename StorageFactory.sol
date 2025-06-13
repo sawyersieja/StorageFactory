4bfb5a83d9a4c2561268c3712116529976a090c6
Sawyer <121059512+sawyersieja@users.noreply.github.com>
2025-06-12 20:42:41 -0700

    Create StorageFactory.sol
    Adding StorageFactory.sol from Storage Factory lesson.

---
+++ StorageFactory.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+//import SimpleStorage from "./SimpleStorage";
+
+import { SimpleStorage } from "./SimpleStorage.sol";
+
+contract StorageFactory {
+
+    // uint256 public favoriteNumber
+    // type visibility name
+    SimpleStorage[] public listOfSimpleStorageContracts;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        // Address
+        // ABI 
+        listOfSimpleStorageContracts[_simpleStorageIndex].store(_newSimpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256){
+        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
+    }
+}
